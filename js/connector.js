e1085e3265eef48ef9217a3e24d1c3535d26b3c9
yesh-ALLFED <yeshodhara@allfed.info>
2022-07-27 17:03:13 +0200

    connector initial setup and html initial

---
+++ js/connector.js
@@ -0,0 +1,13 @@
+console.log('Hi Trello, this time is the charm');
+
+window.TrelloPowerUp.initialize({
+    'card-buttons': function(t, opts){
+        // return array for card buttons
+        return[];
+
+
+    }
+
+
+
+})
