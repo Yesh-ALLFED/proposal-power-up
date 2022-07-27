bc874b88730343b514c54cc644b2f6b1f3ec2ace
yesh-ALLFED <yeshodhara@allfed.info>
2022-07-27 21:37:27 +0200

    Google API initialization

--- js/connector.js
+++ js/connector.js
@@ -1,11 +1,29 @@
 console.log('Hi Trello, this time is the charm');
 
+const GBaseurl = 'https://script.googleapis.com';
+const GoogleAuth = require('google-auth-library');
+const google = require ('googleapis');
+const apikey = 'AIzaSyB9jbjcvVti5MKMtY7VitpjAKBT1LekPLM';
+const scriptid = '1n_h0DEx-wh6JQ5L3Vc-kPluukH-OsxXBq0FQsn0ER73jbPqgOkfz6_w2';
+const auth = new GoogleAuth({scopes: 'https://www.googleapis.com/auth/drive'});
+const script = google.script({version: 'v1', auth});
+
+// make the API request
+
+script.scripts.run({
+    auth: auth,
+    resource: {function: 'trelloMap'},
+    scriptId: scriptid,
+});
+
 var onBtnClick = function (t, opts) {
    // return t.popup({
         //title: 'Does it pop'
 
     //});
-    console.log('Someone clicked the button');
+    //console.log('Someone clicked the button');
+
+
   };
 
 window.TrelloPowerUp.initialize({
