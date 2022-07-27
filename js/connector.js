cac00f94290c4dbe79a8f6bbf1c7b99ed42711a2
yesh-ALLFED <yeshodhara@allfed.info>
2022-07-27 17:44:48 +0200

    update to connector - Returning sample card button

--- js/connector.js
+++ js/connector.js
@@ -3,7 +3,10 @@ console.log('Hi Trello, this time is the charm');
 window.TrelloPowerUp.initialize({
     'card-buttons': function(t, opts){
         // return array for card buttons
-        return[];
+        return[{
+            text: 'Send Proposal'
+            
+        }];
 
 
     }
