a2523af3eddb0dac4e009916d12908a8117b33e0
yesh-ALLFED <yeshodhara@allfed.info>
2022-07-27 18:07:16 +0200

    checking popups

--- js/connector.js
+++ js/connector.js
@@ -1,11 +1,15 @@
 console.log('Hi Trello, this time is the charm');
 
+var onBtnClick = function (t, opts) {
+    console.log('Someone clicked the button');
+  };
+
 window.TrelloPowerUp.initialize({
     'card-buttons': function(t, opts){
         // return array for card buttons
         return[{
             text: 'Send Proposal'
-            
+            callback: onBtnClick,
         }];
 
 
