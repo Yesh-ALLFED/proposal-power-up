0eb0fa844e12013e69783b8fe752813c99984932
Yesh-ALLFED <109857652+Yesh-ALLFED@users.noreply.github.com>
2022-07-28 15:14:40 +0200

    commenting out old var

--- js/connector.js
+++ js/connector.js
@@ -1,12 +1,12 @@
 console.log('Hi Trello, this time is the charm');
 
-const GBaseurl = 'https://script.googleapis.com';
-const GoogleAuth = require('google-auth-library');
-const google = require ('googleapis');
-const apikey = 'AIzaSyB9jbjcvVti5MKMtY7VitpjAKBT1LekPLM';
-const scriptid = '1n_h0DEx-wh6JQ5L3Vc-kPluukH-OsxXBq0FQsn0ER73jbPqgOkfz6_w2';
-const auth = new GoogleAuth({scopes: 'https://www.googleapis.com/auth/drive'});
-const script = google.script({version: 'v1', auth});
+//const GBaseurl = 'https://script.googleapis.com';
+//const GoogleAuth = require('google-auth-library');
+//const google = require ('googleapis');
+//const apikey = 'AIzaSyB9jbjcvVti5MKMtY7VitpjAKBT1LekPLM';
+//const scriptid = '1n_h0DEx-wh6JQ5L3Vc-kPluukH-OsxXBq0FQsn0ER73jbPqgOkfz6_w2';
+//const auth = new GoogleAuth({scopes: 'https://www.googleapis.com/auth/drive'});
+//const script = google.script({version: 'v1', auth});
 
 // make the API request
 
