4c54186127e9060a8ee7828baf891ab1465087f4
yesh-ALLFED <yeshodhara@allfed.info>
2022-08-01 23:28:27 +0200

    Accessing Rest API

--- js/connector.js
+++ js/connector.js
@@ -1,4 +1,7 @@
 console.log('Hi Trello, this time is the charm');
+const Myboard = '62a19391324232461d89f644';
+const KeyToken = 'key=bdaef946dd920b9c14777e0251465bf3&token=b4e71ff4727a2aa33363b42b0b4bd02867dd71218239dbb094dbec9d2676e30e';
+
 
 
 var onBtnClick = function (t, opts) {
@@ -6,42 +9,18 @@ var onBtnClick = function (t, opts) {
         //title: 'Does it pop'
 
         //});
-    var https = require('follow-redirects').https;
-    var fs = require('fs');
+        var cardQueryCb = function(result){
+            console.log(result);
+          }
+          var cardQ = 'https://api.trello.com/1/boards/{{MyBoard}}/customFields/?{{KeyToken}}';
+          fetch(cardQ).then(function(response) {
+            return response.json();
+          }).then(function(data) {
+            console.log(data);
+          });
 
-    var options = {
-      'method': 'POST',
-      'hostname': 'script.googleapis.com',
-      'path': '/v1/scripts/AKfycbzu4YjIw8BlfYk_B-5ouEMMlfuHm9rPoE32hJ0OafWL1YcLVL2rE709rvCKUZhDeMd9tA:run',
-      'headers': {
-        'Authorization': 'Bearer ya29.A0AVA9y1u0Y0fY8OumUsYzavQHq0bqlgIgDdCixoAAt9KRmMjpUeXi5K7PX8FQ6nZt9QfQzFVoeI_3DtxrfDRn2Bd2AVuRc81K3pgDApDu-xLfwvNyJZuewdmvw8ObPyI1zeHIQZqC7il9I6b8NcZ2icdUU-39YUNnWUtBVEFTQVRBU0ZRRTY1ZHI4T1Jmc29ScGRuVlN4dUlxTDB4aGVsUQ0163',
-        'Content-Type': 'application/json'
-      },
-      'maxRedirects': 20
-    };
-
-    var req = https.request(options, function (res) {
-      var chunks = [];
-
-      res.on("data", function (chunk) {
-        chunks.push(chunk);
-      });
-
-      res.on("end", function (chunk) {
-        var body = Buffer.concat(chunks);
-        console.log(body.toString());
-      });
-
-      res.on("error", function (error) {
-        console.error(error);
-      });
-    });
-
-    var postData =  "{\r\n  \"function\": \"testing\",\r\n \r\n}";
-
-    req.write(postData);
-
-    req.end();
+          return t.cards('id', 'name')
+          .then(cardQueryCb);
     
     console.log('Someone clicked the button');
   };
