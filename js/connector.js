666dfd55a539a899db3067f45588cba7d59776cd
yesh-ALLFED <yeshodhara@allfed.info>
2022-08-01 23:53:16 +0200

    rest api update

--- js/connector.js
+++ js/connector.js
@@ -1,5 +1,5 @@
 console.log('Hi Trello, this time is the charm');
-const Myboard = '62a19391324232461d89f644';
+const MyBoard = '62a19391324232461d89f644';
 const KeyToken = 'key=bdaef946dd920b9c14777e0251465bf3&token=b4e71ff4727a2aa33363b42b0b4bd02867dd71218239dbb094dbec9d2676e30e';
 
 
@@ -9,34 +9,16 @@ var onBtnClick = function (t, opts) {
         //title: 'Does it pop'
 
         //});
-    var https = require('follow-redirects').https;
-    var fs = require('fs');
+        var url = 'https://api.trello.com/1/boards/{{MyBoard}}/customFields/?{{KeyToken}}';
+        let response = await fetch(url);
 
-    var options = {
-    'method': 'GET',
-    'hostname': 'api.trello.com',
-    'path': '/1/boards/62a19391324232461d89f644/customFields/?key%253Dbdaef946dd920b9c14777e0251465bf3%2526token%253Db4e71ff4727a2aa33363b42b0b4bd02867dd71218239dbb094dbec9d2676e30e=null',
-    };
-
-    var req = https.request(options, function (res) {
-    var chunks = [];
-
-    res.on("data", function (chunk) {
-        chunks.push(chunk);
-    });
-
-    res.on("end", function (chunk) {
-        var body = Buffer.concat(chunks);
-        console.log(body.toString());
-    });
-
-    res.on("error", function (error) {
-        console.error(error);
-    });
-    });
-
-    req.end();
-    console.log('Someone clicked the button');
+        if (response.ok){
+            let json = await response.json();
+        } else {
+            alert(response.status);
+        }
+    
+        console.log('Someone clicked the button');
   };
 
 window.TrelloPowerUp.initialize({
